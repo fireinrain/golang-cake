94c4e87755802ee682036b316a41832b9220d763
sunrise <575563079@qq.com>
2023-05-04 09:00:37 +0800

    add pages

--- clashsub-manager/frontend/clashsub-manager-ui/src/App.jsx
+++ clashsub-manager/frontend/clashsub-manager-ui/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
-import LoginPage from "./Login.jsx";
+import LoginPage from "./pages/login/Login.jsx";
+import NotFoundPage from "./pages/error/NotFound.jsx";
+import ServerErrorPage from "./pages/error/ServerError.jsx";
+import MaintenancePage from "./pages/error/Maintenance.jsx";
 
 function App() {
 
   return (
     <>
       <LoginPage></LoginPage>
+      <NotFoundPage></NotFoundPage>
+      <ServerErrorPage></ServerErrorPage>
+      <MaintenancePage></MaintenancePage>
+
     </>)
 }
 
